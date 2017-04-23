975773c93f57c80b6a0b0fc3f26dc74bab40e4e9
rkodde <renekodde@gmail.com>
2017-04-23 21:55:55 +0200

    video

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-    echo 'Jo';
-?>
+<html>
+<head>
+    <title>I said hey!</title>
+</head>
+<body>
+    <style>
+        body {
+            margin: 0;
+            background: #000;
+        }
+        video {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            min-width: 100%;
+            min-height: 100%;
+            width: auto;
+            height: auto;
+            z-index: -100;
+            transform: translateX(-50%) translateY(-50%);
+            background:
+    </style>
+    <video id="bgvid" playsinline autoplay loop>
+        <source src="he.mp4" type="video/mp4">
+    </video>
+</body>
+</html>
